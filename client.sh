dd798964205eabd8199d90325774408db5688220
Hadi Amjad <hadiyoville25@gmail.com>
2022-08-01 22:01:26 -0400

    shell files

---
+++ client.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/haadi/Desktop/ASE-22/MixScripts/webpage-crawler-extension && python sele.py
+cd /Users/haadi/Desktop/ASE-22/MixMethods/webpage-crawler-extension && python sele.py
---
+++ server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /Users/haadi/Desktop/ASE-22/Control/webpage-crawler-extension/server &&  node server.js&
+cd /Users/haadi/Desktop/ASE-22/MixScripts/webpage-crawler-extension/server && node server.js&
+cd /Users/haadi/Desktop/ASE-22/MixMethods/webpage-crawler-extension/server && node server.js&
