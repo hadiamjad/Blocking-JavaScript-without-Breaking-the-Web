e95eaad6f4d7a30f4858109962c3f53d8468a7ce
hadiamjad <hadiyoville25@gmail.com>
2023-03-22 15:25:59 -0400

    read-me

--- ALL/webpage-crawler-extension/client.sh
+++ ALL/webpage-crawler-extension/client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 sele.py && python3 label.py && python3 -W ignore countRequests.py && python3 countTags.py
+mkdir server/output && python3 sele.py && python3 label.py && python3 -W ignore countRequests.py && python3 countTags.py
--- Control/webpage-crawler-extension/client.sh
+++ Control/webpage-crawler-extension/client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 sele.py && python3 label.py && python3 SBFL.py && python3 -W ignore countRequests.py
+mkdir ../../Figures && mkdir server/output && python3 sele.py && python3 label.py && python3 SBFL.py && python3 -W ignore countRequests.py
